3a3473b91c8fdbb5166df0bef71def184bc195b6
yzk <yzk365@qq.com>
2021-05-09 08:16:16 +0800

    part of VideoRemoteController

---
+++ DemoApp/VideoRemoteController/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace VideoRemoteController
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.miOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.miShowUrl = new System.Windows.Forms.ToolStripMenuItem();
+            this.ofdOpenVideo = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miOpen,
+            this.miShowUrl});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // miOpen
+            // 
+            this.miOpen.Image = ((System.Drawing.Image)(resources.GetObject("miOpen.Image")));
+            this.miOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.miOpen.Name = "miOpen";
+            this.miOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.miOpen.Size = new System.Drawing.Size(155, 22);
+            this.miOpen.Text = "&Open";
+            // 
+            // miShowUrl
+            // 
+            this.miShowUrl.Name = "miShowUrl";
+            this.miShowUrl.Size = new System.Drawing.Size(155, 22);
+            this.miShowUrl.Text = "Show URL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem miOpen;
+        private System.Windows.Forms.ToolStripMenuItem miShowUrl;
+        private System.Windows.Forms.OpenFileDialog ofdOpenVideo;
+    }
+}
+
---
+++ DemoApp/VideoRemoteController/Form1.cs
@@ -0,0 +1,94 @@
+﻿using LibVLCSharp.Shared;
+using LibVLCSharp.WinForms;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Windows.Forms;
+
+namespace VideoRemoteController
+{
+    public partial class Form1 : Form
+    {
+        private LibVLC libVLC;
+        private MediaPlayer mediaPlayer;
+        private IWebHost host;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.miOpen.Click += MiOpen_Click;
+            this.miShowUrl.Click += MiShowUrl_Click;
+
+            //https://codesailer.com/tutorials/simple_video_player/
+            /*NuGet: LibVLCSharp.WinForms、VideoLAN.LibVLC.Windows 
+             */
+            Core.Initialize();
+            this.libVLC = new LibVLC();
+            this.mediaPlayer = new MediaPlayer(libVLC);
+
+            VideoView videoView = new VideoView();
+            videoView.Dock = DockStyle.Fill;
+            this.Controls.Add(videoView);
+
+            videoView.MediaPlayer = mediaPlayer;
+            mediaPlayer.Play(new Media(libVLC, @"F:\珍贵音频视频\缝纫机乐队 不再犹豫.mp4"));
+
+            host = new WebHostBuilder()
+               .UseKestrel()
+               .UseUrls("http://*:80")
+               .Configure(Configure)
+               .Build();
+            host.RunAsync();
+            this.Closed += Form1_Closed;
+        }
+
+        private void Form1_Closed(object sender, EventArgs e)
+        {
+            host.StopAsync();
+            host.WaitForShutdown();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDefaultFiles();
+            //需要把wwwroot目录中的所有文件设置为“如果较新则复制”
+            app.UseStaticFiles();
+
+            app.Run(async (context) =>
+            {
+                var request = context.Request;
+                var response = context.Response;
+                string path = request.Path.Value;
+                if (path == "/report")
+                {
+                    response.StatusCode = 200;
+                    string value = request.Query["value"];
+                    this.BeginInvoke(new Action(() => {
+                        
+                    }));
+                    await response.WriteAsync("OK");
+                }
+                else
+                {
+                    response.StatusCode = 404;
+                }
+            });
+        }
+
+        private void MiShowUrl_Click(object sender, System.EventArgs e)
+        {
+            FormUrl form = new FormUrl();
+            form.ShowDialog();
+        }
+
+        private void MiOpen_Click(object sender, System.EventArgs e)
+        {
+            if(ofdOpenVideo.ShowDialog()!= DialogResult.OK)
+            {
+                return;
+            }
+            mediaPlayer.Play(new Media(libVLC, ofdOpenVideo.FileName));
+        }
+    }
+}
