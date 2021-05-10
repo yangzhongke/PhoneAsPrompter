4ab75965c355e5048486804d97773b3a7b045878
yzk <yzk365@qq.com>
2021-05-10 20:06:25 +0800

    part of PhoneAsPrompter

---
+++ DemoApp/PhoneAsPrompter/FormUrl.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace VideoRemoteController
+{
+    partial class FormUrl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUrl = new System.Windows.Forms.LinkLabel();
+            this.imgQRCode = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgQRCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelUrl
+            // 
+            this.labelUrl.AutoSize = true;
+            this.labelUrl.Location = new System.Drawing.Point(12, 9);
+            this.labelUrl.Name = "labelUrl";
+            this.labelUrl.Size = new System.Drawing.Size(43, 17);
+            this.labelUrl.TabIndex = 0;
+            this.labelUrl.TabStop = true;
+            this.labelUrl.Text = "_______";
+            // 
+            // imgQRCode
+            // 
+            this.imgQRCode.Location = new System.Drawing.Point(12, 41);
+            this.imgQRCode.Name = "imgQRCode";
+            this.imgQRCode.Size = new System.Drawing.Size(500, 304);
+            this.imgQRCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgQRCode.TabIndex = 1;
+            this.imgQRCode.TabStop = false;
+            // 
+            // FormUrl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 361);
+            this.Controls.Add(this.imgQRCode);
+            this.Controls.Add(this.labelUrl);
+            this.Name = "FormUrl";
+            this.Text = "FormUrl";
+            this.Load += new System.EventHandler(this.FormUrl_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgQRCode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel labelUrl;
+        private System.Windows.Forms.PictureBox imgQRCode;
+    }
+}
---
+++ DemoApp/PhoneAsPrompter/FormUrl.cs
@@ -0,0 +1,32 @@
+﻿using SkiaSharp;
+using SkiaSharp.QrCode.Image;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace VideoRemoteController
+{
+    public partial class FormUrl : Form
+    {
+        public FormUrl()
+        {
+            InitializeComponent();
+        }
+
+        private void FormUrl_Load(object sender, EventArgs e)
+        {
+            string url = "http://" + Environment.MachineName;
+            this.labelUrl.Text = url;
+
+            var qrCode = new QrCode(url, new Vector2Slim(256, 256), SKEncodedImageFormat.Png);
+            using (MemoryStream stream = new MemoryStream())
+            {
+                qrCode.GenerateImage(stream);
+                stream.Position = 0;
+                imgQRCode.SizeMode = PictureBoxSizeMode.Zoom;
+                imgQRCode.Image = Image.FromStream(stream);
+            }
+        }
+    }
+}
